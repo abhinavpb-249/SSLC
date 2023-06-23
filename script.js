f4edf39f4b43ab3e2524c7e0a806a7662773735d
abhinavpb-249 <abhinavpb249@gmail.com>
2023-06-23 11:36:10 +0530

    Update

--- script.js
+++ script.js
@@ -33,45 +33,105 @@
  
  }
  */
+  const grade_input = document.getElementById("grade-input"); 
+  const classic_input = document.getElementById("classic-input"); 
+  const grade_result = document.getElementById("grade-result"); 
+  const classic_result = document.getElementById("classic-result"); 
+  const radio_division = document.getElementsByClassName("input-selector")[0];
+  const classic_radio = document.getElementById("classic-radio");
+  const grade_radio = document.getElementById("grade-radio");
+  const details = document.getElementById("details");
+
+
+//show grade-input when window loads and hides classic-input
+  if(grade_radio.checked){
+   grade_input.style.display="block";
+   classic_input.style.display="none";
+
+}else if (classic_radio.checked) {
+classic_input.style.display="block";
+grade_input.style.display="none";
+}
+
+
+
+radio_division.addEventListener("click",radiochange)
+
+function radiochange(){
+
+   if(grade_radio.checked){
+      grade_input.style.display="block";
+      classic_input.style.display="none";
+
+   }else if (classic_radio.checked) {
+      classic_input.style.display="block";
+      grade_input.style.display="none";
+ }
+
+}
+
+
+function new_output(){
+   grade_result.style.display="block";
+
+
+   S1  = document.getElementById('S1').value;
+   S2  = document.getElementById('S2').value;
+   S3  = document.getElementById('S3').value;
+   S4  = document.getElementById('S4').value;
+   S5  = document.getElementById('S5').value;
+   S6  = document.getElementById('S6').value;
+   S7  = document.getElementById('S7').value;
+   S8  = document.getElementById('S8').value;
+   S9  = document.getElementById('S9').value;
+   S10 = document.getElementById('S10').value;
+
+   
+
+
+S1 = parseInt(S1);
+S2 = parseInt(S2);
+S3  = parseInt(S3);
+S4  = parseInt(S4);
+S5  = parseInt(S5);
+S6  = parseInt(S6);
+S7  = parseInt(S7);
+S8  = parseInt(S8);
+S9  = parseInt(S9);
+S10  = parseInt(S10);
+
+
+finalresult = (S1+S2+S3+S4+S5+S6+S7+S8+S9+S10)/10;
+ 
+finalresult = finalresult.toFixed();  
+
+finalresult = parseInt(finalresult);
+
+finalresult=finalresult+"%-"+(finalresult+10)+"%";
+
+grade_result.innerHTML = finalresult;
+
+}
      
 
+
 function output(){
-   document.getElementById('result').style.display="block";
+   classic_result.style.display="block";
 
 
-     Ap = document.getElementById('A+').value;
-     Ag = document.getElementById('A').value;
-     Bp = document.getElementById('B+').value;
-     Bg = document.getElementById('B').value;
-     Cp = document.getElementById('C+').value;
-     Cg = document.getElementById('C').value;
-     Dp = document.getElementById('D+').value;
-     Dg = document.getElementById('D').value;
-     Eg = document.getElementById('E').value;
-  
-  
-  if (Ap.length ==0 )
-     {Ap=0}
-  if (Ag.length ==0 )
-     {Ag=0}
-  if (Bp.length ==0 )
-     {Bp=0}
-  if (Bg.length ==0 )
-     {Bg=0}
-  if (Cp.length ==0 )
-     {Cp=0}
-  if (Cg.length ==0 )
-     {Cg=0}
-  if (Dp.length ==0 )
-     {Dp=0}
-  if (Dg.length ==0 )
-     {Dg=0}
-  if (Eg.length ==0 )
-     {Eg=0}
-  
+   Ap = document.getElementById('A+').value;
+   Ag = document.getElementById('A').value;
+   Bp = document.getElementById('B+').value;
+   Bg = document.getElementById('B').value;
+   Cp = document.getElementById('C+').value;
+   Cg = document.getElementById('C').value;
+   Dp = document.getElementById('D+').value;
+   Dg = document.getElementById('D').value;
+   Eg = document.getElementById('E').value;
+    
   
   Ap = parseFloat(Ap);
-  Ag = parseFloat(Ag);
+  Ag = parseFloat(Ag);document.getElementById('classic')
   Bp = parseFloat(Bp);
   Bg = parseFloat(Bg);
   Cp = parseFloat(Cp);
@@ -80,6 +140,7 @@ function output(){
   Dg = parseFloat(Dg);
   Eg = parseFloat(Eg);
   
+ 
   
    total = Ap+Ag+Bp+Bg+Cp+Cg+Dp+Dg+Eg;
    console.log("total:  "+total);
@@ -91,27 +152,27 @@ function output(){
 
 finalresult = finalresult.toFixed();  
 
-finalresult=(finalresult-10)+"%-"+finalresult;
+finalresult=(finalresult-10)+"%-"+finalresult+"%";
 
 if(total==10){
 
-             document.getElementById('result').innerHTML = finalresult+"%";
+             classic_result.innerHTML = finalresult;
   
              
               if ( (parseInt(Dg)+ parseInt(Eg))<10  )
                                   { 
-                                  document.getElementById("details").innerHTML = "You have passed the SSLC examination" ;
-                                    document.getElementById('result').style.color="green"; 
+                                  details.innerHTML = "You have passed the SSLC examination" ;
+                                    classic_result.style.color="green"; 
                                   jsConfetti.addConfetti() ;
                                    }
-               else {document.getElementById("details").innerHTML = "You have to apppear for the SAY exam to be eligible for higher   studies"; }
+               else {details.innerHTML = "You have to apppear for the SAY exam to be eligible for higher   studies"; }
                
                clicked= true; //used to check before sharing. The finalresult is only shared if this condition is satisfied
                
               }
   
-else   {document.getElementById('result').innerHTML = "error";
-       document.getElementById("details").innerHTML = "Total number of subjects doesn't add upto 10" ;
+else  { classic_result.innerHTML = "error";
+         details.innerHTML = "Total number of subjects doesn't add upto 10" ;
        } 
   
 
